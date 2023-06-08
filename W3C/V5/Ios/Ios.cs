1e994f26c58bddb10e1ad19dab9e990910de3a19
Pradum Kumar <pradumraj360@gmail.com>
2023-06-08 18:43:15 +0530

    version bump and adding example project for appium.webdriver v5 (#4)

--- W3C/V5/Ios/Ios.cs
+++ W3C/V5/Ios/Ios.cs
@@ -21,24 +21,24 @@ namespace csharp_appium__w3c_first_ios_test_browserstack
       percyOtions.Add("ignoreErrors", "true");
       percyOtions.Add("enabled", "true");
       // Adding capabilities
-      capabilities.AddAdditionalCapability("bstack:options", browserstackOptions);
-      capabilities.AddAdditionalCapability("appium:percyOptions", percyOtions);
+      capabilities.AddAdditionalAppiumOption("bstack:options", browserstackOptions);
+      capabilities.AddAdditionalAppiumOption("appium:percyOptions", percyOtions);
       // Adding Device
-      capabilities.AddAdditionalCapability("os_version", "16");
-      capabilities.AddAdditionalCapability("appium:deviceName", "iPhone 14");
-      // Adding app that was uploaded
-      capabilities.AddAdditionalCapability("appium:app", "<APP_URL>");
-      // Project details
-      capabilities.AddAdditionalCapability("project", "First CSharp W3C Project");
-      capabilities.AddAdditionalCapability("build", "CSharp IOS");
-      capabilities.AddAdditionalCapability("name", "first_test");
+      capabilities.App = "<APP_URL>";
+      capabilities.AddAdditionalAppiumOption("bstack:options", browserstackOptions);
+      capabilities.AddAdditionalAppiumOption("appium:percyOptions", percyOtions);
+      // Adding Device
+      capabilities.DeviceName = "iPhone 14";
+      capabilities.PlatformVersion = "16";
+      capabilities.AddAdditionalAppiumOption("project", "First CSharp W3C Project");
+      capabilities.AddAdditionalAppiumOption("build", "CSharp IOS");
+      capabilities.AddAdditionalAppiumOption("name", "first_test");
 
       // Initialize the remote Webdriver using BrowserStack remote URL
       // and desired capabilities defined above
-      IOSDriver<AppiumWebElement> driver = new IOSDriver<AppiumWebElement>(
+      AppiumDriver driver = new IOSDriver(
               new Uri("https://hub-cloud.browserstack.com/wd/hub"), capabilities);
 
-      // Initialize AppPercy
       AppPercy appPercy = new AppPercy(driver);
       try
       {
@@ -50,7 +50,6 @@ namespace csharp_appium__w3c_first_ios_test_browserstack
       }
       //Taking first screenshot
       appPercy.Screenshot("First Screenshot");
-
       var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
       var searchElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(MobileBy.AccessibilityId("Text Button")));
       searchElement.Click();
